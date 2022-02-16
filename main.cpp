df601ba7454e5da7952ce2cc6540bfccf1d7c08c
SiddhartaDutta <siddhartadutta99@gmail.com>
2022-02-16 11:47:54 -0800

    Try-catch + switch implementation started

--- main.cpp
+++ main.cpp
@@ -5,6 +5,41 @@ using namespace std;
 
 int main(){
 
+    while(1){
+
+        // Print UI
+        cout << "Linear Algebra Calculator\n";
+        cout << "Create Matrix\n";
+        cout << "Row Reduce Matrix\n";
+        cout << "Find Solutions of a Linear System\n";
+        cout << "Print Matrix\n";
+        cout << "Display Matrix Pivots\n";
+
+        string inputStr;
+        int inputInt;
+
+        try{
+
+            cin >> inputStr;
+            inputInt = stoi(inputStr);
+                
+            switch(inputInt){
+
+                case 1:
+                    break;
+
+                default:
+                    return;
+
+            }
+
+        } catch(...){
+            cout << "Error: Invalid Input\n";
+        }
+
+
+    }
+
     int n, m;
 
     cout << "Rows: ";
